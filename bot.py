b08cd567c63b2bb6568a8a541129c9a77f77a235
edwardoboh <obohedward@gmail.com>
2021-11-16 22:36:33 +0100

    Created Bot command for a dice game

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import os
-import random
 from discord.ext import commands
 from dotenv import load_dotenv
+import random
 
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
@@ -11,5 +11,26 @@ bot = commands.Bot(command_prefix="!")
 @bot.event
 async def on_ready():
     print(f'{bot.user} has connected to Discord')
+
+@bot.command(name="99", help="Command to invoke the nine-niner")
+async def nine_niner(context):
+    niner = [
+        "Niner has been invoked by the lords",
+        "Oracle Nine-niner will now come in",
+        (
+            "Let nine niner take over "
+            "who called the niners?"
+        )
+    ]
+    await context.send(random.choice(niner))
+
+@bot.command(name="roll_dice", help="Ask bot to roll dice a number of times")
+async def roll_dice(ctx, dice_faces: int, dice_rolls: int):
+    dice_outcome = [
+        random.choice(range(1, dice_faces + 1))
+        for _ in range(dice_rolls)
+    ]
+    print(f'Dice Outcomes:: {dice_outcome}')
+    await ctx.send(dice_outcome)
 
 bot.run(TOKEN)
