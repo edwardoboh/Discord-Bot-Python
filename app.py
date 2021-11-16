7781d7696d7256f38a6343fc027f249f468dfb4b
edwardoboh <obohedward@gmail.com>
2021-11-16 14:59:36 +0100

    Added message event handler

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 import discord
 from dotenv import load_dotenv
+import random
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -16,12 +17,29 @@ async def on_ready():
         f'{client.user} is connected to the following guild:\n'
         f'{guild.name} (id: {guild.id})'
         )
-    members = "\n - ".join([member.name for member in guild.members])
+    # members = "\n - ".join([member.name for member in guild.members])
     # print(f'Guild members in {guild.name}:: \n {members}')
 
 @client.event
 async def on_member_join(member):
     await member.create_dm()
     await member.dm_channel.send(f'Hi {member.name}, Welcome to my discord server')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    random_messages = [
+        'I\'m the human form of the 💯 emoji.',
+        'Bingpot!',
+        (
+            'Cool. Cool cool cool cool cool cool cool ',
+            'no doubt no doubt no doubt no doubt.'
+        ),
+    ]
+    if(message.content == "99!"):
+        response = random.choice(random_messages)
+        await message.channel.send(response)
+    pass
 
 client.run(TOKEN)
